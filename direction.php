1be96aed75150656aa65500f4c657e37d45acd8f
takashima <k014c1125@it-neec.jp>
2016-09-05 10:28:38 +0900

    add a line

---
+++ direction.php
@@ -0,0 +1,91 @@
+<?php
+    $error="";
+    $base_url = 'http://linedesign.cloudapp.net/hoshimiru/constellation';
+    $query = ['lat'=>0, 'lng'=>0 ,'date'=>'2016-01-01', 'hour'=>0, 'min'=>0, 'disp'=>'on' ];
+    $proxy = array(
+      "http" => array(
+       "proxy" => "tcp://proxy.kmt.neec.ac.jp:8080",
+       'request_fulluri' => true,
+      ),
+    );
+    $proxy_context = stream_context_create($proxy);
+    $response = file_get_contents(
+                      $base_url.'?' .
+                      http_build_query($query),
+                      false,
+                      $proxy_context
+                );
+    $result = json_decode($response,true);
+
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>星座の方向</title>
+</head>
+<p><h1>     星座の方角      </h1></p>
+
+<tr>
+  <p> <td><input type="text" name="name" size="30" maxlength="20"></td><td>
+    <input type="submit" value="現在位置"></p>
+<p><select name="見たい星座">
+      <?php
+for ($i = 0; $i < 88; $i++) {
+  ?>
+          <option><?= $result["result"][$i]["jpName"] ?></option>
+<?php
+}
+?>
+</select></p>
+
+
+
+
+
+
+<select name="month">
+  <script>
+    var i;
+    for(i=1; i<=12; i++){
+  	  document.write('<option value="i">'+i+'月</option>');
+    }
+  </script>
+
+</select>
+
+
+
+
+  <select name="day">
+  <script>
+
+    var i;
+    for(i=1; i<=31; i++){
+  	  document.write('<option value="i">'+i+'日</option>');
+    }
+  </script>
+
+</select>
+
+
+
+
+<select>
+
+  <script>
+    var i;
+    for(i=0; i<=23; i++){
+  	  document.write('<option value="i">'+i+'時</option>');
+    }
+  </script>
+
+  </script>
+    </td>
+ </tr>
+
+<body>
+
+</body>
+    </html>
